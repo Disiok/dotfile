b753ba8aca05e519a19ff7c15469da48ae09af9f
Disiok <simonsdsuo@gmail.com>
2015-04-11 13:16:06 -0400

    Change some settings

--- .bash_profile
+++ .bash_profile
@@ -1,6 +1,2 @@
-
-PATH="~/Development/android-sdk-macosx/platform-tools:${PATH}"
-export PATH
-
 PATH="/usr/local/bin:${PATH}"
 export PATH
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,14 +21,6 @@ function remove-orig
 	echo All files removed
 end
 
-function repo
-	~/Dropbox/Waterloo\ 2014-2015/Academics\ 2A/CS246/repo/1149
-end
-
-function project
-	~/Dropbox/Waterloo\ 2014-2015/Academics\ 2A/CS246/chamber-crawler
-end
-
 function testfile
 	mvim test{$argv}.in
 	mvim test{$argv}.out	
@@ -47,8 +39,4 @@ end
 
 function ssh-es-slave-1
 	ssh ec2-user@ec2-54-200-255-167.us-west-2.compute.amazonaws.com
-end
-
-function ssh-waterloo
-	ssh -Y sdsuo@linux.student.cs.uwaterloo.ca
 end
