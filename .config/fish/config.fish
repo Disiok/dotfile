207243eea154ee4c584e62a0f0e858b78cbcfc82
Disiok <simonsdsuo@gmail.com>
2016-03-21 23:10:11 -0700

    Use oh-my-fish to manager fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,42 +1,8 @@
-set fish_greeting ""
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/Disiok/.local/share/omf"
 
-set -xg ANDROID_HOME /usr/local/opt/android-sdk
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/Disiok/.config/omf"
 
-function show-files
-	defaults write com.apple.finder AppleShowAllFiles YES 
-	killall Finder /System/Library/CoreServices/Finder.app
-end
-
-function hide-files
-	defaults write com.apple.finder AppleShowAllFiles NO
-	killall Finder /System/Library/CoreServices/Finder.app
-end
-
-function get-screen
-	adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > screen.png
-end
-
-function remove-orig
-	find ./ -name "*.orig" -exec rm -i "{}" \;
-	echo All files removed
-end
-
-function testfile
-	mvim test{$argv}.in
-	mvim test{$argv}.out	
-end
-
-function header
-	set upper (echo $argv | tr '[:lower:]' '[:upper:]')
-	echo \#ifndef ___{$upper}_H___ > {$argv}.h
-	echo \#define ___{$upper}_H___ >> {$argv}.h
-	echo \#endif >> {$argv}.h
-end
-
-function ssh-es-master
-	ssh ec2-user@ec2-54-200-221-39.us-west-2.compute.amazonaws.com
-end
-
-function ssh-es-slave-1
-	ssh ec2-user@ec2-54-200-255-167.us-west-2.compute.amazonaws.com
-end
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
